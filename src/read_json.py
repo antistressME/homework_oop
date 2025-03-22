9b62002c8bb33471523accfd1208931e04e6168f
Anastasia <antipinaanastasia0112@gmail.com>
2025-03-22 15:28:53 +0300

    add funk to read json

---
+++ src/read_json.py
@@ -0,0 +1,31 @@
+import json
+
+from src.classes import Category, Product
+
+
+def read_json():
+    """ Получаем данные из json-файла."""
+    with open('products.json', encoding='utf8') as file:
+        data = json.load(file)
+        return data
+
+def get_classes(data):
+    """ Получаем объекты классов"""
+    for point in data:
+        category = Category(point["name"], point["description"], point["products"])
+        # print(category.name)
+        # print(category.description)
+        # print(category.product_count)
+        # print(category.category_count)
+        for item in point["products"]:
+            product = Product(item["name"], item["description"], item["price"], item["quantity"])
+            # print(product.name)
+            # print(product.description)
+            # print(product.price)
+            # print(product.quantity)
+
+
+if __name__ == '__main__':
+    data = read_json()
+    get_classes(data)
+
